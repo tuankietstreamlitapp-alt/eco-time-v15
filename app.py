7cc2dd6fb97b5575e0f5b5c33f26f88d438b2c64
Kiệt Huỳnh <tuankietstreamlitapp@gmail.com>
2026-08-28 14:52:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,23 +103,23 @@ lat1, lon1 = None, None
 diem_don_text = "Vị trí GPS hiện tại của bạn"
 
 # Lựa chọn cấp quyền sử dụng vị trí (Có / Không)
-# cho_phep_gps = st.radio(
-    "📍 Cho phép sử dụng vị trí của bạn?",
+cho_phep_gps = st.radio(
+    #"📍 Cho phép sử dụng vị trí của bạn?",
     options=["Có (Tự động lấy vị trí đón)", "Không (Tắt định vị)"],
     index=0,
     horizontal=True,
 )
 
-# if cho_phep_gps.startswith("Có"):
+if cho_phep_gps.startswith("Có"):
     loc = get_geolocation()
     if loc and "coords" in loc:
         lat1 = loc["coords"]["latitude"]
         lon1 = loc["coords"]["longitude"]
-        st.success("💰 Sô tiền cần thanh toán.")
+        st.success #("💰 Sô tiền cần thanh toán.")
     else:
         st.info(
-            "💡 Trình duyệt đang chờ bạn cấp quyền vị trí. Bấm 'Cho phép' trên"
-            " bảng thông báo của trình duyệt nếu có."
+            #"💡 Trình duyệt đang chờ bạn cấp quyền vị trí. Bấm 'Cho phép' trên"
+            #" bảng thông báo của trình duyệt nếu có."
         )
 else:
     st.warning("⚠️ Bạn đã tắt tính năng định vị vị trí.")
